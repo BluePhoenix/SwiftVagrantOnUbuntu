2981c96096e871bdfe9824b3d47818ddd8450068
Felix Barros <felix.barros@gmail.com>
2015-12-03 23:41:27 -0500

    Add Vagrantfile and use ubuntu/trusty64, and draft initial bootstraps

---
+++ download_swift.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir /vagrant/snapshots/
+cd /vagrant/snapshots/
+curl -O https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12-01-b/swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz
