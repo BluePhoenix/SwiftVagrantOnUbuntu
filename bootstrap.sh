e5cb751619ffeb2ac23ad65038aa1428dfb7cf88
Felix Barros <felix.barros@gmail.com>
2015-12-04 08:37:50 -0500

    Implement downloading of Swift snapshot, extracting, and copying over to system

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
+# set -x
 
+# Set variables for current Swift snapshot
 Snapshot=swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz
-
-mkdir -p /vagrant/snapshots/
-cd /vagrant/snapshots/
+SnapshotURL="https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12-01-b/swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz"
+SnapshotLocation=/vagrant/snapshots
+SwiftExtractionLocation=$SnapshotLocation/swift
 
 # Download Swift snapshot if it does not exist
-if ! [ -f /vagrant/snapshots/$Snapshot ]; then
-  curl -o /vagrant/snapshots/$Snapshot "https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12-01-b/swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz"
+if ! [ -f $SnapshotLocation/$Snapshot ]; then
+	mkdir -p $SnapshotLocation/
+	cd $SnapshotLocation/
+	echo "$SnapshotLocation/$Snapshot missing, will download now."
+	curl -o $SnapshotLocation/$Snapshot $SnapshotURL
+else
+	echo "$SnapshotLocation/$Snapshot already exists."
 fi
 
-# curl -O https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12-01-b/swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz
-# cp /vagrant/snapshots/$Snapshot /tmp/swift.tar.gz
-mkdir -p ~/.swift
-tar -zxvf /vagrant/snapshots/$Snapshot -C ~/.swift --strip-components=1
-PATH=~/.swift/usr/bin:$PATH
+# Extract Swift snapshot if it has not been extracted
+if ! [ -d $SwiftExtractionLocation ]; then
+	mkdir -p $SwiftExtractionLocation/
+	echo "$SwiftExtractionLocation missing, extracting to it now."
+	tar -zxvf /vagrant/snapshots/$Snapshot -C $SwiftExtractionLocation --strip-components=1  
+	echo "$SwiftExtractionLocation extraction complete."
+else
+	echo "$SwiftExtractionLocation already exists."
+fi
+
+# Copy over Swift files
+echo "Copying from $SwiftExtractionLocation to system."
+cp -r $SwiftExtractionLocation/* /
 
 sudo apt-get --assume-yes install clang
