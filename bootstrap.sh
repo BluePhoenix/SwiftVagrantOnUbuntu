f3de659ee2a40d8531377662353e39aec72b53ef
Felix Barros <felix.barros@gmail.com>
2015-12-04 20:29:02 -0500

    Modify bootstrap to update apt-get when provisioning
    Also added (currently commented out) the dependencies needed if we
    wanted to build swift.

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,4 +31,15 @@ fi
 echo "Copying from $SwiftExtractionLocation to system."
 cp -r $SwiftExtractionLocation/* /
 
-sudo apt-get --assume-yes install clang
+# Attempt to update apt-get
+sudo apt-get update
+sudo apt-get upgrade
+
+# Required dependencies listed at https://github.com/apple/swift
+sudo apt-get --assume-yes install git clang
+
+# These are optional, but needed if you want to build Swift yourself
+# sudo apt-get --assume-yes install git cmake ninja-build clang uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config
+# sudo apt-get --assume-yes install clang-3.6
+# sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
+# sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
